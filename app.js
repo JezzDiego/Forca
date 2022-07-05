6af0d0ee5b55abc4d3757fcf42a35d8a1710adc3
JezzDiego <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>
2022-07-05 12:03:40 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,106 @@
+var words = ['olho', 'cebola', 'computador', 'mesa', 'bolsa', 'celular', 'caneta', 'copo', 'mouse', 'teclado', 'pneumoultramicroscopicossilicovulcanoconiotico'];
+var choosenWord;
+var hiddenWord = "";
+var triedLetters = [];
+var correctLetters = [];
+var chances = 6;
+var errors = 0;
+var img = document.getElementById("img")
+
+function drawWord(){
+    choosenWord = words[Math.floor(Math.random() * words.length)]
+}
+
+function hideWord(){
+    for(let i = 0; i<choosenWord.length; i++){
+        hiddenWord += '*';
+    }
+}
+
+
+function verifyAttempt(){
+    let tentativa = document.getElementById("tentativa").value;
+    var mensagem = document.getElementById("mensagem");
+    
+    if(tentativa.length > 1){
+        if(tentativa == choosenWord)
+            mensagem.textContent = "Você acertou!";
+        else
+            mensagem.textContent = "Você errou!";
+    } else {
+        verifyWord(tentativa);
+    }
+    updateScreen();
+    document.getElementById("tentativa").value = "";
+}
+
+
+function updateScreen(){
+    var palavraVisivel = "";
+    for(let i = 0; i<choosenWord.length; i++){
+        if(correctLetters.includes(choosenWord[i])){
+            palavraVisivel += choosenWord[i];
+        }else {
+            palavraVisivel += '*';
+        }
+    }
+
+    document.getElementById("palavra-secreta").textContent = palavraVisivel;
+}
+
+function drawImage(){
+    let button = document.getElementById("tentar")
+    switch(errors){
+        case 1:
+            img.src = "./sprites/sprites(2).png";
+            break;
+        case 2:
+            img.src = "./sprites/sprites(3).png";
+            break;
+        case 3:
+            img.src = "./sprites/sprites(4).png";
+            break; 
+        case 4:
+            img.src = "./sprites/sprites(5).png";
+            break;
+        case 5:
+            img.src = "./sprites/sprites(6).png";
+            break;
+        case 6:
+            img.src = "./sprites/sprites(7).png";
+            button.setAttribute("disabled","disabled")
+            mensagem.innerText = "Você perdeu já tio!";
+            break
+    }
+}
+
+function verifyWord(tentativa){
+    if(!triedLetters.includes(tentativa)){
+        triedLetters.push(tentativa);
+    } else {
+        mensagem.textContent = "Você já tentou essa letra.";
+        return;
+    }
+
+    for(let i = 0; i<choosenWord.length; i++){
+        if(tentativa == choosenWord[i]){
+            correctLetters.push(tentativa);
+            var exists = true;
+        }
+    }
+
+
+    if(!exists){
+        errors++;
+        drawImage();
+
+    }
+}
+
+function start(){
+    drawWord();
+    hideWord();
+    updateScreen();
+}
+
+start();
